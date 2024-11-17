f19e1fa1304208173217975bc4e489a145af9aa1
Marharyta <paduchak.pn@ucu.edu.ua>
2024-11-17 12:53:58 +0200

    start for work with algorithms

---
+++ algorithm.py
@@ -0,0 +1,15 @@
+"""
+Algorithms for working with a graph
+"""
+def dfs():
+    pass
+
+def merge_components():
+    pass
+
+def spanning_tree_prima():
+    pass
+
+if __name__=='__main__':
+    import doctest
+    print(doctest.testmod())
