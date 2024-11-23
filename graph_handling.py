238b91af2f7d33f812e93d046bf924bac2bb240e
Teodor Talan <135043435+oToTak@users.noreply.github.com>
2024-11-23 14:34:55 +0200

    +read_map with city_roads_names

--- graph_handling.py
+++ graph_handling.py
@@ -18,6 +18,17 @@ City = namedtuple("City", ("roads", "is_center"))
 # is_center: bool, True when city is central
 
 
+def city_roads_names(city, file_listed) -> list:
+    """
+    Find all names of roads connected to the city.
+
+    :param city: str, city name
+    :param file_listed: list, list of file lines
+
+    :returns: list[str], list of all road names connected to the city
+    """
+    return [line[:line.find(',')] for line in file_listed[1:] if city in line]
+
 def read_map(path: str) -> Map:
     """
     Read map data from the file at the given path.
@@ -26,7 +37,27 @@ def read_map(path: str) -> Map:
 
     :returns: Map
     """
-    # with open(path, 'r', encoding='utf-8') as f:
+    roads, cities = {}, {}
+
+    with open(path, 'r', encoding='utf-8') as file:
+        listed_file = file.readlines()
+
+        center_city = listed_file[0].strip()
+        center_city_roads = city_roads_names(center_city, listed_file)
+        cities.setdefault(center_city, City(roads=center_city_roads, is_center=True))
+
+        for line in listed_file[1:]:
+            road, c1, c2, distnce = line.split(', ')
+
+            distnce = float(distnce.strip())
+            roads.setdefault(road, Road(city1=c1, city2=c2, distance=distnce))
+
+            c1_roads = city_roads_names(c1, listed_file)
+            cities.setdefault(c1, City(c1_roads, is_center=False))
+            c2_roads = city_roads_names(c2, listed_file)
+            cities.setdefault(c2, City(c2_roads, is_center=False))
+        
+    return Map(roads, cities)
 
 
 def read_damaged_roads(path: str) -> dict[str, float]:
