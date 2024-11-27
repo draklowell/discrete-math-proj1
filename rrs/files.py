cb72ae397a5261d76574a9bf876e95c41bf7575a
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2024-11-27 16:14:55 +0200

    Minor refactors for rrs.files

--- rrs/files.py
+++ rrs/files.py
@@ -6,38 +6,39 @@ Module for working with the map in the form of graph.
 from rrs.datatypes import Map, Road, City
 
 
-def get_dot_graph(map_: Map, damaged_roads: dict[str, float]) -> str:
+def write_dot_graph(path: str, map_: Map, damaged_roads: dict[str, float]) -> str:
     """
     Generate .dot graph from the graph data.
 
+    :param path: str, path to the .dot file
     :param map_: Map
     :param damaged_roads: dict[str, float], list of damaged roads
 
     :returns: str, .dot graph
     """
-    data = "graph{\n"
-    for road_name, road in map_.roads.items():
-        if road_name in damaged_roads:
-            data += f'"{road.city1}" -- "{road.city2}" '
-            data += f'[weight={road.distance}; label="{road_name}({damaged_roads[road_name]})"'
-            data += ";color=red]\n"
-        else:
-            data += f'"{road.city1}" -- "{road.city2}"'
-            data += f'[weight={road.distance}; label="{road_name}"]\n'
+    with open(path, "w", encoding="utf8") as file:
+        file.write("graph{\n")
+        for road_name, road in map_.roads.items():
+            if road_name in damaged_roads:
+                file.write(f'"{road.city1}" -- "{road.city2}" ')
+                file.write(f'[weight={road.distance}; label="{road_name}({damaged_roads[road_name]})"')
+                file.write(";color=red]\n")
+            else:
+                file.write(f'"{road.city1}" -- "{road.city2}"')
+                file.write(f'[weight={road.distance}; label="{road_name}"]\n')
+        file.write("}\n")
 
-    return data + "}"
 
-
-def city_roads_names(city, file_listed) -> list:
+def get_city_roads_names(city: str, listed_file: list[str]) -> list[str]:
     """
     Find all names of roads connected to the city.
 
     :param city: str, city name
-    :param file_listed: list, list of file lines
+    :param listed_file: list[str], list of file lines
 
     :returns: list[str], list of all road names connected to the city
     """
-    return [line[: line.find(",")] for line in file_listed[1:] if city in line]
+    return [line[: line.find(",")] for line in listed_file[1:] if city in line]
 
 
 def read_map(path: str) -> Map:
@@ -54,19 +55,19 @@ def read_map(path: str) -> Map:
         listed_file = file.readlines()
 
         center_city = listed_file[0].strip()
-        center_city_roads = city_roads_names(center_city, listed_file)
+        center_city_roads = get_city_roads_names(center_city, listed_file)
         cities.setdefault(center_city, City(roads=center_city_roads, is_center=True))
 
         for line in listed_file[1:]:
-            road, c1, c2, distnce = line.split(", ")
+            road, city1, city2, distnce = line.split(", ")
 
             distnce = float(distnce.strip())
-            roads.setdefault(road, Road(city1=c1, city2=c2, distance=distnce))
+            roads.setdefault(road, Road(city1=city1, city2=city2, distance=distnce))
 
-            c1_roads = city_roads_names(c1, listed_file)
-            cities.setdefault(c1, City(c1_roads, is_center=False))
-            c2_roads = city_roads_names(c2, listed_file)
-            cities.setdefault(c2, City(c2_roads, is_center=False))
+            city1_roads = get_city_roads_names(city1, listed_file)
+            cities.setdefault(city1, City(city1_roads, is_center=False))
+            city2_roads = get_city_roads_names(city2, listed_file)
+            cities.setdefault(city2, City(city2_roads, is_center=False))
 
     return Map(roads, cities)
 
